9cc5eec5af7493a1a359e0de5365ef03903bd48a
ShadowYukiMiu <116520737+ShadowYukiMiu@users.noreply.github.com>
2022-12-08 21:38:55 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,5 @@
-#ifndef PROVA_CONV_LETT_LIB_H
-#define PROVA_CONV_LETT_LIB_H
-bool conv(char c);
-extern int my_global_int;
-char ris;
-#endif //PROVA_CONV_LETT_LIB_H
+#ifndef CONVERTER_LIB_H
+#define CONVERTER_LIB_H
+char conversione(char x);
+bool checkimput(int x);
+#endif //CONVERTER_LIB_H
