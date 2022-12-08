e358bebe98e2c8caa4cc072d51d344ae6832f574
ShadowYukiMiu <116520737+ShadowYukiMiu@users.noreply.github.com>
2022-12-08 22:18:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,17 +1,19 @@
+//
+// Created by ginev on 08/12/2022.
+//
+
 #include "lib.h"
-bool checkimput(int x){
-    if((x>=65&&x<=90)||(x>=97 && x<=122)) {
+bool checkimput(int x) {
+    if((x>=65&&x<=90)||(x>=97 && x<=122))
         return true;
-    }else{
+    else
         return false;
-    }
 }
-char conversione(char x){
+char conversione(char x) {
     char ris;
-        if (x >= 'A' && x <= 'Z') {
-            ris= x + 32;
-        }else if (x >= 'a' && x <= 'z') {
-            ris= x - 32;
-        return ris;
-        }
+    if (x >= 'A' && x <= 'Z')
+        ris = x + 32;
+    else if (x >= 'a' && x <= 'z')
+        ris = x - 32;
+    return ris;
 }
