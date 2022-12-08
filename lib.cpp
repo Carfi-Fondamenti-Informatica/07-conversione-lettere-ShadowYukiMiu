bcd2b55f3e2f7154fe70211f280f55cffc10ac82
ShadowYukiMiu <116520737+ShadowYukiMiu@users.noreply.github.com>
2022-12-08 21:15:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
+using char ris;
 
+bool conv(char c) {
+    if (c >= 'a' && c <= 'z') {
+        ris = c-32;
+        return true;
+    }else if (c >= 'A' && c <= 'Z') {
+        ris = c+32;
+        return true;
+    }else{
+        return flase;
+    }
+  }
