a2a7936cb3c6c782110c8d04b421d6aef5cb6efc
ShadowYukiMiu <116520737+ShadowYukiMiu@users.noreply.github.com>
2022-12-08 21:21:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+extern char ris; 
 
 int main(){
+  char a;
+  char b;
+  cout << "inserire un carattere:" << endl;
+  cin >> a;
+  b= conv(a);
+  if (b==true) {
+      cout << ris << endl;
+      }else{
+      cout << "errore" << endl;
+  }
   return 0;
 }
