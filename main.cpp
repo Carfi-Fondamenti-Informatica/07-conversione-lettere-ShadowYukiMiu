df9510e8a4c49e43b7695fbae33ea137a61c68a7
ShadowYukiMiu <116520737+ShadowYukiMiu@users.noreply.github.com>
2022-12-08 22:19:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,15 @@
 #include "lib.h"
 using namespace std;
 
-int main(){
-  char c;
-  char v;
-  cin>>c;
-  if (checkimput (x: c)) {
-      v= conversione(x: c);
-      cout<<v;
-  }else{
-      cout<<"errore"<<endl;
-  }
+int main() {
+   char c;
+   char v;
+   cin >> c;
+   if (checkimput (x: c)) {
+       v= conversione(x: c);
+       cout<<v;
+   }else {
+       cout << "errore" << endl;
+   }
   return 0;
 }
